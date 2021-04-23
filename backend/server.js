ef0eb7acb422240679e3fe6635595975034c6550
anitkumarsingh <anikumarsingh99@gmail.com>
2021-04-23 20:01:20 +0530

    single product api endpoint

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,19 @@ app.get('/', (req, res, next) => {
 });
 
 app.get('/api/products', (req, res, next) => {
+	res.status(200);
 	res.json(products);
+});
+
+app.get('/api/product/:id', (req, res, next) => {
+	const product = products.find((p) => p._id === req.params.id);
+	if (!product) {
+		res.status(404);
+		res.send({ message: 'Product not found!' });
+	} else {
+		res.status(200);
+		res.json(product);
+	}
 });
 
 app.listen(PORT, () => {
