c71f29a818bb33dafa6d7fc87789777cff28263a
anitkumarsingh <anikumarsingh99@gmail.com>
2021-04-24 08:04:34 +0530

    added product route

---
+++ backend/routes/products.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import Products from '../models/products.js';
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+	try {
+		const products = await Products.find({});
+		if (products) {
+			res.status(200).json(products);
+		} else {
+			res.status(404).json({ message: 'Product not found!' });
+		}
+	} catch (error) {
+		console.error(`Error ${error.message}`);
+	}
+});
+
+router.get('/:id', async (req, res, next) => {
+	try {
+		const product = await Products.find({ _id: req.params.id });
+		if (product) {
+			res.status(200).json(product);
+		} else {
+			res.status(404).json({ message: 'Product not found!' });
+		}
+	} catch (error) {
+		console.error(`Error ${error.message}`);
+	}
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import products from './mocks/products.js';
+import productsRouter from './routes/products.js';
 import dotenv from 'dotenv';
 import connectDB from './config/index.js';
 
@@ -9,24 +9,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use('/api/products', productsRouter);
+
 app.get('/', (req, res, next) => {
 	res.send('Hello World!');
-});
-
-app.get('/api/products', (req, res, next) => {
-	res.status(200);
-	res.json(products);
-});
-
-app.get('/api/product/:id', (req, res, next) => {
-	const product = products.find((p) => p._id === req.params.id);
-	if (!product) {
-		res.status(404);
-		res.send({ message: 'Product not found!' });
-	} else {
-		res.status(200);
-		res.json(product);
-	}
 });
 
 app.listen(PORT, () => {
