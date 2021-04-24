09e3c9d6203402d1271897d8f5b7c814aa1e3ca9
anitkumarsingh <anikumarsingh99@gmail.com>
2021-04-24 15:12:00 +0530

    added controller and refactor

---
+++ backend/controllers/products.js
@@ -0,0 +1,23 @@
+import Products from '../models/products.js';
+import AsyncHandler from 'express-async-handler';
+
+const getProducts = AsyncHandler(async (req, res, next) => {
+	const products = await Products.find({});
+	if (products) {
+		res.status(200).json(products);
+	} else {
+		res.status(404).json({ message: 'Product not found!' });
+	}
+});
+
+const getProduct = AsyncHandler(async (req, res, next) => {
+	const product = await Products.findById(req.params.id);
+	if (product) {
+		res.status(200).json(product);
+	} else {
+		res.status(404);
+		throw new Error({ message: 'Product not found!' });
+	}
+});
+
+export { getProduct, getProducts };
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,32 +1,9 @@
 import express from 'express';
-import Products from '../models/products.js';
-import AsyncHandler from 'express-async-handler';
+import { getProduct, getProducts } from '../controllers/products.js';
 
 const router = express.Router();
 
-router.get(
-	'/',
-	AsyncHandler(async (req, res, next) => {
-		const products = await Products.find({});
-		if (products) {
-			res.status(200).json(products);
-		} else {
-			res.status(404).json({ message: 'Product not found!' });
-		}
-	})
-);
-
-router.get(
-	'/:id',
-	AsyncHandler(async (req, res, next) => {
-		const product = await Products.findById(req.params.id);
-		if (product) {
-			res.status(200).json(product);
-		} else {
-			res.status(404);
-			throw new Error({ message: 'Product not found!' });
-		}
-	})
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProduct);
 
 export default router;
